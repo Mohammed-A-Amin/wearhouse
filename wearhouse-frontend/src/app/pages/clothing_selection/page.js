582cb3d5ba81af5a36fa644a0951f433924f5104
Damon Huynh <dhuynh10072007@gmail.com>
2025-12-04 16:30:53 -0800

    fetch items from backend in closet; refactor code
    moved around some code from file to file bc there
    were some redudant parts

--- wearhouse-frontend/src/app/pages/clothing_selection/page.js
+++ wearhouse-frontend/src/app/pages/clothing_selection/page.js
@@ -6,6 +6,7 @@ import Dropdown from "../../components/Dropdown";
 import Navigation from "../../components/Navigation";
 import Image from "next/image";
 import { useAuth } from "@clerk/nextjs";
+import ItemGrid from "@/app/components/ItemGrid";
 
 const MOCK_HISTORY = ["Hoodie", "Sneakers", "Jeans", "Jacket", "Hat"];
 
@@ -173,37 +174,3 @@ function SearchHistory({ history, setQuery }) {
   );
 }
 
-function ItemGrid({ items, selected, toggleSelect }) {
-  const baseUrl = "http://assets.myntassets.com/v1/images/style/properties/"
-  return (
-    <div className="grid grid-cols-4 gap-4">
-      {items.map((item) => {
-        const isSelected = selected.has(item.id);
-        return (
-          <button
-            key={item.id}
-            onClick={() => toggleSelect(item.id)}
-            className={`relative bg-orange-100 border border-orange-300 hover:shadow flex flex-col justify-between h-[320px]`}
-          >
-            <img 
-            src={item.imageLink}
-            alt={item.productDisplayName}
-            className="object-cover w-full h-64"
-            />
-            <div className="px-2 py-1 text-left text-sm text-gray-700">
-              <div className="font-medium">{item.productDisplayName}</div>
-              <div className="text-xs text-gray-500">{item.masterCategory}</div>
-              <div className="text-xs text-gray-500">{item.gender}</div>
-            </div>
-
-            {isSelected && (
-              <div className="absolute top-1 right-1 bg-blue-500 text-white w-6 h-6 flex items-center justify-center rounded-full text-xs">
-                ✓
-              </div>
-            )}
-          </button>
-        );
-      })}
-    </div>
-  );
-}
